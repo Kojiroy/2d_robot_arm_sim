1d24317c7a7be22d0cb29cf94642025b0e2fcb41
Yamada <Yamada>
2023-11-01 14:31:31 -0700

    Made the rectangle into a polygon

--- main.py
+++ main.py
@@ -16,18 +16,17 @@ fpsClock = pygame.time.Clock()
 
 pygame.display.set_caption("Robot Arm")
 
-test_limb = RobotJoint("test", np.array([150,150]), np.array([0,0]), np.array([10,30]), np.array([DISPLAY_WIDTH,DISPLAY_HEIGHT]))
-robot_limbs = [test_limb]
+test_limb = RobotJoint(name="test", pos=np.array([150,150]), origin=np.array([0,0]), size=np.array([10,30]), boundary=np.array([DISPLAY_WIDTH,DISPLAY_HEIGHT]))
 
-x_vel = 1
-y_vel = 2
+test_limb.set_vel(np.array([1,2]))
+
+robot_limbs = [test_limb]
 
 while running:
     # Update limb states
     fpsClock.tick(60)
     for limb in robot_limbs:
-        x_vel *= 1 if limb.move(x_vel, 0) else -1
-        y_vel *= 1 if limb.move(0, y_vel) else -1
+        limb.update_state(1)
 
     display.fill(BG)
     for event in pygame.event.get():
@@ -36,7 +35,8 @@ while running:
     # pygame.draw.rect(display, ARM_COLOR, pygame.Rect(50, 30, 20, 60), 5)
 
     for limb in robot_limbs:
-        pygame.draw.rect(display, ARM_COLOR, limb.rect)
+        limb.draw(display)
+        # pygame.draw.rect(display, ARM_COLOR, limb.rect)
     pygame.display.update()
     display.fill(BG)
 pygame.quit()
--- robot_joint.py
+++ robot_joint.py
@@ -1,54 +1,65 @@
 import numpy as np
 import math
-from pygame import Rect, draw
+from pygame import Rect, draw, surface
 
 from utils import find_nearest, find_lowest, find_highest
 
 class PolyRect:
     # A rectangle made out of polygon lines. Makes it easier to abstract this system to
     # matplotlib.
-    def __init__(self, pos : np.ndarray([0,0]), size : np.ndarray([0,0]), rotation: np.ndarray([0,0]), color : np.array([10,180,200, 200])):
+    def __init__(self, pos : np.ndarray([0,0]), size : np.ndarray([0,0]), rotation: float(0), color : np.array([10,180,200, 200])):
         self.pos = pos
         self.size = size
         self.rotation = rotation # in radians
         self.color = color
-        self.points = np.zeros((4,2), dtype=float)
+        self.points = np.zeros((4,2), dtype=float) # Points are [top_right, bottom_left, top_left, bottom_right] Default settings for polygons, not my choice lol
         self.update_points()
 
-    def rotate(self, val : float = 0.0, degrees : bool = True) -> bool:
+    def rotate(self, val : float = 0.0, degrees : bool = True) -> bool: # TODO: Add rotation relative to specific origin
         # Returns True if rotations 
         self.rotation = math.radians(val) if degrees else val
 
-    def move_ip(self, x: float = 0, y: float = 0):
-        self.pos[0] = x; self.pos[1] = y
+    def move_ip(self, x: float = 0, y: float = 0) -> None:
+        self.pos[0] += x; self.pos[1] += y
 
-    def update_points(self):
-        x:float = self.pos[0] + self.size[0]
-        y:float = self.pos[1] + self.size[1]
-        h:float = 0
+    def update_points(self) -> None:
+        h:float = math.hypot(self.size[1],self.size[0])
         feta:float = 0.0
+        # print(f"Pos: {self.pos}\nSize: {self.size}")
         for i in range(4):
-            x *= -1 if i%2 else 1
-            y *= -1 if i/2 else 1
-            h = math.hypot(x,y)
-            feta = math.atan(y/x) + self.rotation
-            self.points[i] = [h*math.cos(feta), h*math.cos(feta)]
+            # print(f"Printing state of corner[{i}]")
+            feta = math.atan((self.size[1])/self.size[0]) + self.rotation + (math.pi if i%3 else 0)
+            feta *= -1 if i%2 else 1
+            # print(feta)
+            self.points[i] = [h*math.cos(feta) + self.pos[0], h*math.sin(feta) + self.pos[1]]
+            # print(self.points[i])
 
-    def draw(self, screen):
-        print(f"Screen type: {type(screen)}")
+    def draw(self, screen:surface.Surface) -> None:
+        # print(f"Screen type: {type(screen)}")
         self.update_points()
         draw.polygon(screen, self.color, self.points)
+    
+    def get_corners(self) -> np.ndarray:
+        return self.points
 
 class RobotJoint: # Has no image
-    def __init__(self, name : str = "unamed_joint", pos : np.array = np.array([0,0]), origin : np.array = np.array([0,0]), size : np.array = np.array([1,1]), boundary : np.array=np.array([350,350])) -> None:
+    def __init__(self, name : str = "unamed_joint", pos : np.array = np.array([0,0]), rotation: float = 0, origin : np.array = np.array([0,0]), size : np.array = np.array([1,1]), color: np.array=([10,180,200, 200]), boundary : np.array=np.array([350,350])) -> None:
         self.name = name
-        self.origin = origin
+        self.origin = origin # Origin of rotation
         self.boundary = boundary
-        self.rect = Rect(pos[0], pos[1], size[0], size[1])
+        self.rect = PolyRect(pos, size, rotation, color)
+        self.state = np.array([[pos[0],pos[1]], [0,0], [0,0]], dtype=float) # [pos, vel, accel]
     
+    def set_vel(self, vel : np.array([0,0])) -> None:
+        self.state[1] = vel
+
     def rotate(self, degrees : float = 0.0) -> None: # TODO: Complete Rotation
         # Rotates an object relative to its current relative position
         pass
+
+    def update_state(self, time_step : float = 1) -> None:
+        self.state[0] += self.state[1] * time_step
+        self.move(self.state[0][0], self.state[0][1])
 
     def move(self, x : float = 0, y : float = 0) -> bool: # Tested
         # Return false if it hits something, true if movement is safe.
@@ -61,26 +72,38 @@ class RobotJoint: # Has no image
         # Correct Movement
         # Returns True if it moves fine. False otherwise
 
-        corners = np.asarray([self.rect.topleft, self.rect.topright, self.rect.bottomleft, self.rect.bottomright])
-        print(corners)
-        print(type(corners))
+        # corners = np.asarray([self.rect.topleft, self.rect.topright, self.rect.bottomleft, self.rect.bottomright])
+        corners = self.rect.get_corners()
+        # print(corners)
+        # print(type(corners))
         lowest_x = find_lowest(corners[:, 0])
         lowest_y = find_lowest(corners[:, 1])
         highest_x = find_highest(corners[:, 0])
         highest_y = find_highest(corners[:, 1])
         if lowest_x < 0 :
             x = -lowest_x # Becomes positive
+            self.state[1][0] *= -1 if self.state[1][0] < 0 else 1
             hit = True
+            print(f"Hit left side with {lowest_x}")
         elif highest_x > self.boundary[0]:
             x = -highest_x + self.boundary[0] # Becomes negative
+            self.state[1][0] *= -1 if self.state[1][0] > 0 else 1
             hit = True
+            print(f"Hit right side with {highest_x}")
         
         if lowest_y < 0:
             y = -lowest_y # becomes positive
+            self.state[1][1] *= -1 if self.state[1][1] < 0 else 1
             hit = True
+            print(f"Hit floor with {lowest_y}")
         elif highest_y > self.boundary[1]:
             y = -highest_y + self.boundary[1] # Becomes negative
+            self.state[1][1] *= -1 if self.state[1][1] > 0 else 1
             hit = True
+            print(f"Hit ceil with {highest_x}")
         
         self.rect.move_ip(x,y)
         return not hit
+    
+    def draw(self, screen):
+        self.rect.draw(screen)
